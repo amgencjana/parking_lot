b5b59c97cae73d3193bba1610b26a8d39dc290e1
amgencjana <adam.goryczka@gmail.com>
2018-12-10 12:56:50 +0800

    Commands::SlotNumberForRegistration

---
+++ lib/parking_lot/commands/slot_number_for_registration.rb
@@ -0,0 +1,32 @@
+module ParkingLot
+  module Commands
+    class SlotNumberForRegistration
+
+     class << self
+        def match(cmd)
+          cmd.include? 'slot_number_for_registration_number'
+        end    
+      end
+
+      attr_accessor :command_txt, :command, :license_plate
+
+      def initialize(command_txt)
+        @command_txt     = command_txt
+        @license_plate   = get_license_plate
+      end
+
+      def execute
+        parking = Parking.current
+
+        query = ParkingLot::Services::ParkingQuery.new(parking, license_plate: license_plate)
+        query.slot_number_by_license_plate
+      end
+
+      private 
+        def get_license_plate
+          color = command_txt.split(' ').last
+        end
+
+    end
+  end
+end
